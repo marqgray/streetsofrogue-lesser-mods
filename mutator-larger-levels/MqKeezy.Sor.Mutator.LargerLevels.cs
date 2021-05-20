e51910997b4410511e50d1250ad35621896349c2
Marq Gray <marquis.s.sandifer@gmail.com>
2021-05-19 22:34:07 -0500

    Added Larger Levels project

---
+++ mutator-larger-levels/MqKeezy.Sor.Mutator.LargerLevels.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using HarmonyLib;
+using mqKeezy_Mutator_LargerLevels.Properties;
+
+namespace mqKeezy_Mutator_LargerLevels
+{
+    [BepInPlugin(ModInfo.BepInExPluginId, ModInfo.Title, ModInfo.Version)]
+    public class MqkSorMutatorLargerLevels : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            new Harmony(ModInfo.BepInExHarmonyPatchesId).PatchAll();
+        }
+    }
+}
---
+++ mutator-larger-levels/Properties/Mod.Info.cs
@@ -0,0 +1,12 @@
+﻿namespace mqKeezy_Mutator_LargerLevels.Properties
+{
+    public static class ModInfo
+    {
+        public const string Title = "Mutator: Larger Levels";
+        public const string Description = "Streets of Rogue BepInEx plugin";
+        public const string Author = "mqKeezy";
+        public const string Version = "1.0.0.0";
+        public const string BepInExPluginId = "mqKeezy.Sor.Mutator.LargerLevels";
+        public const string BepInExHarmonyPatchesId = "mqKeezy.Sor.Mutator.LargerLevels.Harmony";
+    }
+}
