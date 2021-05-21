a871b05b0fdc4513d9250d6ce0d3fa036a2faac2
Marq Gray <marquis.s.sandifer@gmail.com>
2021-05-21 01:03:52 -0500

    Initial commit of Multiple Disasters mod

---
+++ mutator-multiple-disasters/MqKeezy.Sor.Mutator.MultipleDisasters.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using HarmonyLib;
+using mqKeezy_Mutator_MultipleDisasters.Properties;
+using RogueLibsCore;
+
+namespace mqKeezy_Mutator_MultipleDisasters
+{
+    [BepInPlugin(ModInfo.BepInExPluginId, ModInfo.Title, ModInfo.Version)]
+    public class MqkSorMutatorMultipleDisasters : BaseUnityPlugin
+    {
+        public static CustomMutator Mutator;
+
+        private void Awake()
+        {
+            new Harmony(ModInfo.BepInExHarmonyPatchesId).PatchAll();
+        }
+    }
+}
---
+++ mutator-multiple-disasters/Properties/Mod.Info.cs
@@ -0,0 +1,12 @@
+﻿namespace mqKeezy_Mutator_MultipleDisasters.Properties
+{
+    public static class ModInfo
+    {
+        public const string Title = "Mutator: Multiple Disasters";
+        public const string Description = "Streets of Rogue BepInEx plugin";
+        public const string Author = "mqKeezy";
+        public const string Version = "1.0.0.0";
+        public const string BepInExPluginId = "mqKeezy.Sor.Mutator.MultipleDisasters";
+        public const string BepInExHarmonyPatchesId = "mqKeezy.Sor.Mutator.MultipleDisasters.Harmony";
+    }
+}
