d2a035bf5cfd264a666bba88562dfb1b1efba023
Marq Gray <marquis.s.sandifer@gmail.com>
2021-05-21 14:52:57 -0500

    Initial commit of Crowded Levels mod

---
+++ mutator-crowded-levels/MqKeezy.Sor.Mutator.CrowdedLevels.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using HarmonyLib;
+using mqKeezy_Mutator_CrowdedLevels.Properties;
+using RogueLibsCore;
+
+namespace mqKeezy_Mutator_CrowdedLevels
+{
+    [BepInPlugin(ModInfo.BepInExPluginId, ModInfo.Title, ModInfo.Version)]
+    public class MqkSorMutatorCrowdedLevels : BaseUnityPlugin
+    {
+        public static CustomMutator Mutator;
+
+        private void Awake()
+        {
+            Mutator = RogueLibs.CreateCustomMutator(id: "mqKeezy.CrowdedLevels",
+                unlockedFromStart: true,
+                new CustomNameInfo(english: "Crowded Levels"),
+                new CustomNameInfo(english: ""));
+
+            new Harmony(ModInfo.BepInExHarmonyPatchesId).PatchAll();
+        }
+    }
+}
---
+++ mutator-crowded-levels/Properties/Mod.Info.cs
@@ -0,0 +1,12 @@
+﻿namespace mqKeezy_Mutator_CrowdedLevels.Properties
+{
+    public static class ModInfo
+    {
+        public const string Title = "Mutator: Crowded Levels";
+        public const string Description = "Streets of Rogue BepInEx plugin";
+        public const string Author = "mqKeezy";
+        public const string Version = "1.0.0.0";
+        public const string BepInExPluginId = "mqKeezy.Sor.Mutator.CrowdedLevels";
+        public const string BepInExHarmonyPatchesId = "mqKeezy.Sor.Mutator.CrowdedLevels.Harmony";
+    }
+}
